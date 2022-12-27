2ae1c858e78cd9ae0ec5a31517ad25a4c3503879
Apple <apple@Apples-iMac.local>
2022-12-27 03:11:44 +0300

    Done

---
+++ function/ServiceBusQueueGo/__init__.py
@@ -0,0 +1,68 @@
+import logging
+import azure.functions as func
+import psycopg2
+import os
+from datetime import datetime
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import *
+
+def main(msg: func.ServiceBusMessage):
+
+    notification_id = int(msg.get_body().decode('utf-8'))
+    logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
+
+    # TODO: Get connection to database
+    con=psycopg2.connect(host="postgress1server.postgres.database.azure.com",
+                         database='techconfdb',
+                         user='abinm670@postgress1server',
+                         password='Ahmed1934$'
+                         )
+    logging.info(f"Connected to Database Successfully")
+
+    
+        # TODO: Get notification message and subject from database using the notification_id
+    try:
+        cor=con.cursor()
+        cor.execute("SELECT message, subject from notification WHERE id=%s;",(notification_id,))
+        messageContent, subject=cor.fetchone()
+        logging.info(f"Message and Subject ID Notfication '{messageContent}':{subject}")
+        
+        
+        
+        #     # TODO: Get attendees email and name
+        cor.execute("SELECT email, first_name, last_name from attendee;")
+        attendees=cor.fetchall()
+        # for i in attendees:
+            # logging.info(f"list of emails '{i[0]}', and First Name '{i[1]}', last name:'{i[2]}'")
+        
+
+
+        #     # TODO: Loop through each attendee and send an email with a personalized subject
+        for x in attendees:
+            message= Mail(
+                from_email="jeddah_nona_1980@hotmail.com",
+                to_emails='jeddah_nona_1980@hotmail.com',
+                subject='User{}:{}'.format(x[2], subject),
+                #logging.info(f"Okay '{subject}'")
+                plain_text_content=messageContent
+                )
+            try:
+                req=SendGridAPIClient('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')
+                respond=req.send(message)
+                print(respond.body)
+                print(respond.status_code)
+            except Exception as e:
+                print(e)
+        
+                
+        
+                
+        #     # TODO: Update the notification table by setting the completed date and updating the status with the total number of attendees notified
+            total=len(attendees)
+            cor.execute("UPDATE notification SET status = %s WHERE id=%s;", (total,notification_id))
+            con.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+         logging.error(error)
+    finally:
+    #     # TODO: Close connection`
+         con.close()
---
+++ web/config.py
@@ -0,0 +1,25 @@
+import os
+
+app_dir = os.path.abspath(os.path.dirname(__file__))
+
+class BaseConfig:
+    DEBUG = True
+    POSTGRES_URL="postgress1server.postgres.database.azure.com"  #TODO: Update value
+    POSTGRES_USER="abinm670@postgress1server" #TODO: Update value
+    POSTGRES_PW="Ahmed1934$"   #TODO: Update value
+    POSTGRES_DB="techconfdb"   #TODO: Update value
+    DB_URL = 'postgresql://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
+    CONFERENCE_ID = 1
+    SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://techconf.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=BwcoSp8acVkYHMUpZ3QY2f+hO44rI3c6uKfDTeYD0QY=' #TODO: Update value
+    SERVICE_BUS_QUEUE_NAME ='techconfque'
+    ADMIN_EMAIL_ADDRESS:'jeddah_nona_1980@hotmail.com'
+    SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
+
+class DevelopmentConfig(BaseConfig):
+    DEBUG = True
+
+
+class ProductionConfig(BaseConfig):
+    DEBUG = False
